533687caccdd55841ebf1b2a46622657dcd0dca4
Zia Gondal <89640018+ziabinumer@users.noreply.github.com>
2025-12-30 21:37:03 +0500

    revise clienthandler calls

--- server/ServerMain.java
+++ server/ServerMain.java
@@ -3,21 +3,22 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import auth.AuthService;
 
 public class ServerMain {
-
     private static final int PORT = 5000;
 
-    public static void maiqn(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Server starting on port " + PORT);
+        AuthService authService = new AuthService();
 
-        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                ClientHandler handler = new ClientHandler(clientSocket);
+                ClientHandler handler = new ClientHandler(clientSocket, authService);
                 handler.start();
             }
 
