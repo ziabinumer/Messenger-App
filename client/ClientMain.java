d7bad009adea9e28348590229a1cd82193542f5e
Zia Gondal <89640018+ziabinumer@users.noreply.github.com>
2025-12-30 20:40:33 +0500

    add variable for ports

--- client/ClientMain.java
+++ client/ClientMain.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ClientMain {
+
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 5000;
+
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 5000);
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
             System.out.println("Connected to server");
         } catch (IOException e) {
             System.out.println("Client error: " + e.getMessage());
